bfc41a6d890cb1bb126b170f26f42e52e89a7a39
therealmahmoud <moha91875460@gmail.com>
2023-09-08 23:18:12 +0000

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints all alphabet letters
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char i;
+for (i = 'a'; i <= 'z'; i++)
+	putchar (i);
+	putchar('\n');
+	return (0);
+}
