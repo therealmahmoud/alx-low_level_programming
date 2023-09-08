64d00cdb7b85903b199cf940155aeadff5785ef8
therealmahmoud <moha91875460@gmail.com>
2023-09-08 00:12:36 +0000

    this is my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,17 @@
-#include <unisdt.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
  * Descriptiom: print a quote using write function
  * write(int fd,const void *buf,size_t count)
  *
- * Return: 1 (not success)
+ * return: 1 (not success)
 */
 
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-		write(1.quo.59);
+
+	write(1.quo.59);
 	return (1);
 }
