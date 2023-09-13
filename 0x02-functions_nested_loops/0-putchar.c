cb7ce210171ab81e0e72a164e8e4a183b30df747
therealmahmoud <moha91875460@gmail.com>
2023-09-13 00:53:07 +0000

    this is the commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,8 +13,8 @@ int main(void)
 	char str[] = "_putchar";
 	int ch;
 
-	for (ch = 0; ch <= 7; ch++)
+	for (ch = 0; ch < 8; ch++)
 		_putchar(str[ch]);
-	_putchar("\n");
+	_putchar('\n');
 	return (0);
 }
