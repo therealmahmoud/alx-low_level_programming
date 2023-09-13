ca7d5e94de55bf131d8547d11f0a960cd3a91ba7
therealmahmoud <moha91875460@gmail.com>
2023-09-13 00:42:25 +0000

    this is the commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,16 @@
 /**
  * main - Entry point
  *
- * Descriptiom: print a quote using write function
+ * Description: print a quote using write function
  * write(int fd,const void *buf,size_t count)
  *
- * return: 1 (not success)
+ * return: 1 (fail)
 */
 
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1,quo,59);
+	write(1, quo, 59);
 	return (1);
 }
