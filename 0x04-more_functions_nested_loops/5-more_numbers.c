5c6d9901fbf3eddc43e9240ef17581631893f288
therealmahmoud <moha91875460@gmail.com>
2023-09-14 23:31:16 +0000

    this is the commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - nmbr fct
+ *
+ * Return: Always 0 (success)
+ */
+
+void more_numbers(void)
+{
+	int num, row, count;
+
+	for (row = 0; row <= 10; row++)
+	{
+		for (count = 0; count <= 14; count++)
+		{
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
+		}
+		_putchar('\n');
+	}
+
+
+}
