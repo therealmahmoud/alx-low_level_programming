82274fbe103ecc44f45d34ebfc05b2884d63cf6f
therealmahmoud <moha91875460@gmail.com>
2023-09-14 22:58:05 +0000

    this is the commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,12 @@
 void print_most_numbers(void)
 {
 	int num;
-		for (num = '0'; num <= '9'; num++)
+
+	for (num = 0; num <= 9; num++)
 	{
-		if (num == '2' || num == '4')
-				continue;
-			_putchar(num + 48);
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(num + 48);
 	}
 	_putchar('\n')
 }
