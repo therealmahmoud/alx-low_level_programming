f3aae3cf0779bad05054e78e4862b9fbe358929a
therealmahmoud <moha91875460@gmail.com>
2023-09-14 22:32:12 +0000

    this is the commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,11 @@
 
 void print_numbers(void)
 {
-	char c;
-	
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
