22786afdbd083debbeda63019f2d06288f005f0b
therealmahmoud <moha91875460@gmail.com>
2023-09-29 00:43:02 +0000

    this is my commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: string to print
+ *return: void
  */
 void _puts_recursion(char *s)
 {
@@ -12,6 +12,9 @@ void _puts_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
+	else
+	{
 	_putchar(*s);
-	_puts_recursion(s + 1);	
+	_puts_recursion(s + 1);
+	}
 }
