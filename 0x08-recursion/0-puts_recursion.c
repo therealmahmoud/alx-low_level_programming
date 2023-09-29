4f132df18edefa9c82bb4e358529f6318a68ef5f
therealmahmoud <moha91875460@gmail.com>
2023-09-29 01:38:50 +0000

    this is my commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse
- * @s: string to print
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
  */
-void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_putchar(*s);
-		_print_rev_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
+
+	else
+		_putchar('\n');
 }
