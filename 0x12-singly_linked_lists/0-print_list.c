35b48c5eb1850ab1d9d280b9273833eeb2863964
therealmahmoud <moha91875460@gmail.com>
2023-10-20 01:24:42 +0000

    this is my commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,14 +13,14 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
-		n++;
 		h = h->next;
+		n++;
 	}
 		return (n);
 
