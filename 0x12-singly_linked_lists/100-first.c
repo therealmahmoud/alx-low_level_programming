297dff7f819213cbbc5ad385ced72c8f5382aa71
therealmahmoud <moha91875460@gmail.com>
2023-10-20 02:21:35 +0000

    this is my commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,6 @@ void _constructor(void) __attribute__ ((constructor));
  */
 void _constructor(void)
 {
-	puts("You're beat! and yet, you must allow,\n""I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
