33f6f6dd68c4e5175d654fd05bbed4d76c63be0e
therealmahmoud <moha91875460@gmail.com>
2023-10-24 02:40:58 +0000

    this is my commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - a function that prints a linked list
+ * @h: pointer to the head
+ *
+ * Return: number of nodes in linked list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next
+		n++;
+	}
+	return (n);
+}
