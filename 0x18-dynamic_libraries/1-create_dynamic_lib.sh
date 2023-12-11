05ee75749026989a9639a1402fc1847964c3b802
therealmahmoud <moha91875460@gmail.com>
2023-12-11 20:09:22 +0000

    UPDATED

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -c -fpic *.c | gcc -shared -o liball.so *.o | export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -c -fpic *.c | gcc -shared -o liball.so *.o | export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH | gcc -std=gnu89 main.c -L. -lliball
